f8e09d117c39e43cc0997f6f4592ff83694d4412
zyrezia <mr_joseph_rodriguez@yahoo.com>
2025-07-21 11:13:34 -0400

    Initial commit

---
+++ Final Assignment Website/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 1296) {
+            pos = 0;
+        } else {
+            pos++;
+        }
+        elem.style.left = pos + "px";
+    }
+}
